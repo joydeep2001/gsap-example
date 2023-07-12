4a173bcde46a4465e0adcaf9d852e502a9879bbf
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2023-07-13 03:04:35 +0530

    another

--- assets/js/customScroll3.js
+++ assets/js/customScroll3.js
@@ -1,4 +1,111 @@
 gsap.registerPlugin(ScrollTrigger);
+let scrollCount = 0;
+
+const leftColumn = document.querySelector(".left");
+const rightColumn = document.querySelector(".right");
+var current = "blue";
+
+window.addEventListener("keyup", (e) => {
+  if (e.key == "ArrowUp") {
+    //up
+    prev();
+  }
+  if (e.key == "ArrowDown") {
+    //down
+
+    next();
+  }
+});
+
+// document.addEventListener("mousewheel", scroll);
+// document.addEventListener("DOMMouseScroll", scroll);
+
+var w;
+function scroll(e) {
+  clearTimeout(w);
+  //e.preventDefault();
+  var e = window.event || e;
+  w = setTimeout(() => {
+    w = undefined;
+    if (e.wheelDelta < 0 || e.detail > 0) {
+      next();
+    }
+    if (e.wheelDelta > 0 || e.detail < 0) {
+      prev();
+    }
+  }, 100);
+}
+
+function next() {
+  let r, l;
+
+  switch (current) {
+    // case "gold": {
+    //   //to show blue and start over
+    //   l = "0";
+    //   r = "-200";
+    //   current = "blue";
+
+    //   break;
+    // }
+    case "blue": {
+      //to show red
+      l = "-100";
+      r = "-100";
+      current = "red";
+      break;
+    }
+    case "red": {
+      //to show gold
+      l = "-200";
+      r = "0";
+      current = "gold";
+      break;
+    }
+  }
+
+  leftColumn.style.transform = `translateY(${l}vh)`;
+  rightColumn.style.transform = `translateY(${r}vh)`;
+}
+function prev() {
+  let r, l;
+
+  switch (current) {
+    case "red": {
+      //to show blue and start over
+      l = "0";
+      r = "-200";
+      current = "blue";
+      break;
+    }
+    // case "blue": {
+    //   //to show gold
+    //   l = "-200";
+    //   r = "0";
+    //   current = "gold";
+    //   break;
+    // }
+    case "gold": {
+      //to show red
+      l = "-100";
+      r = "-100";
+      current = "red";
+      break;
+    }
+  }
+
+  leftColumn.style.transform = `translateY(${l}vh)`;
+  rightColumn.style.transform = `translateY(${r}vh)`;
+}
+function isScrollUp(currentScroll) {
+  console.log(currentScroll);
+  if (currentScroll > lastScroll + 50) {
+    lastScroll = currentScroll;
+    return true;
+  }
+  return false;
+}
+function isScrollDown() {}
 const timeline1 = gsap.timeline({
   scrollTrigger: {
     trigger: ".aos",
@@ -6,64 +113,65 @@ const timeline1 = gsap.timeline({
     end: "top -100%",
     markers: true,
     pin: true,
-    paused: true,
-    scrub: true,
+
+    onUpdate: function (self) {},
     onEnter: function () {
-      //   document.addEventListener("scroll", scroll);
+      document.addEventListener("mousewheel", scroll);
+      document.addEventListener("DOMMouseScroll", scroll);
     },
     onLeave: function () {
-      // document.removeEventListener("scroll", scroll);
+      document.removeEventListener("mousewheel", scroll);
+      document.removeEventListener("DOMMouseScroll", scroll);
     },
     onLeaveBack: function () {
-      //   document.removeEventListener("scroll", scroll);
+      document.removeEventListener("mousewheel", scroll);
+      document.removeEventListener("DOMMouseScroll", scroll);
     },
     onEnterBack: function () {
-      // document.addEventListener("scroll", scroll);
+      document.addEventListener("mousewheel", scroll);
+      document.addEventListener("DOMMouseScroll", scroll);
     },
   },
 });
 
-const leftY = ["0vh", "100vh", "200vh"];
-const rightY = ["-200vh", "100vh", "0vh"];
-let currPos = 0;
-function scroll(position) {}
-
-timeline1
-
-  .to(
-    ".left",
-    {
-      y: "-100vh",
-      duration: 1,
-    },
-    1
-  )
-  .to(
-    ".left",
-    {
-      y: "-200vh",
-      duration: 2,
-    },
-    3
-  );
-timeline1
-
-  .to(
-    ".right",
-    {
-      y: "-100vh",
-      duration: 1,
-    },
-    1
-  )
-  .to(
-    ".right",
-    {
-      y: "0vh",
-      duration: 2,
-    },
-    3
-  );
+// timeline1
+//   .addLabel("l-step1")
+//   .to(
+//     ".left",
+//     {
+//       y: "-100vh",
+//       duration: 1,
+//     },
+//     1
+//   )
+//   .addLabel("l-step2")
+//   .to(
+//     ".left",
+//     {
+//       y: "-200vh",
+//       duration: 2,
+//     },
+//     3
+//   );
+// timeline1
+//   .addLabel("r-step1")
+//   .to(
+//     ".right",
+//     {
+//       y: "-100vh",
+//       duration: 1,
+//     },
+//     1
+//   )
+//   .addLabel("r-step1")
+//   .to(
+//     ".right",
+//     {
+//       y: "0vh",
+//       duration: 2,
+//     },
+//     3
+//   );
 
 // gsap
 //   .timeline({
